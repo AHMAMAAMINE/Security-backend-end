fc9413763c37e9bc18f4a3d61dede7a9652d6976
AHMAMAAMINE <amineahmama17@gmail.com>
2021-06-18 16:54:33 +0100

    -User -Token

--- src/main/java/com/example/learn/constant/SecurityConstant.java
+++ src/main/java/com/example/learn/constant/SecurityConstant.java
@@ -1,7 +1,7 @@
 package com.example.learn.constant;
 
 public class SecurityConstant{
-    public static final long EXPIRATION_TIME=432_000_000;//expressed by miliseconds
+    public static final long EXPIRATION_TIME=432_000_000;//expressed by milliseconds
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String JWT_TOKEN_HEADER= "Jwt-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED= "Token cannot be verified";
@@ -10,6 +10,6 @@ public class SecurityConstant{
     public static final String AUTHORITIES= "authorities";
     public static final String FORBIDDEN_MESSAGE= "You need to connect to get to this page";
     public static final String ACCESS_DENIED_MESSAGE="you don't have the access for this command";
-    public static final String OPTIONS_HTTP_MEHOD="OPTIONS";
+    public static final String OPTIONS_HTTP_METHOD="OPTIONS";
     public static final String[] PUBLIC_URL={"/user/login","/user/register","/user/resetpassword/**","/user/image/**"};
 }
